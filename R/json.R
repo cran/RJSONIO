0a203a184c8ee4e37a8d2dad05edfa1eba467ace
ORPHANED <cran@R-project.org>
2019-06-04 09:19:07 +0000

    version 1.3-1.2

--- R/json.R
+++ R/json.R
@@ -326,7 +326,7 @@ function(txt)
 {
    txt = paste(as.character(txt), collapse = "\n")
    enc = mapEncoding(Encoding(txt))
-   .Call("R_jsonPrettyPrint", txt, enc, package = "RJSONIO")
+   .Call("R_jsonPrettyPrint", txt, enc, PACKAGE = "RJSONIO")
 }
 
 
--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /* .Call calls */
 extern SEXP R_fromJSON(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP R_isValidJSON(SEXP);
-extern SEXP R_jsonPrettyPrint(SEXP, SEXP, SEXP);
+extern SEXP R_jsonPrettyPrint(SEXP, SEXP);
 extern SEXP R_json_parser_init_from_con(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP R_readFromJSON(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
@@ -14,7 +14,7 @@ extern SEXP R_readFromJSON(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 static const R_CallMethodDef CallEntries[] = {
     {"R_fromJSON",                  (DL_FUNC) &R_fromJSON,                  7},
     {"R_isValidJSON",               (DL_FUNC) &R_isValidJSON,               1},
-    {"R_jsonPrettyPrint",           (DL_FUNC) &R_jsonPrettyPrint,           3},
+    {"R_jsonPrettyPrint",           (DL_FUNC) &R_jsonPrettyPrint,           2},
     {"R_json_parser_init_from_con", (DL_FUNC) &R_json_parser_init_from_con, 5},
     {"R_readFromJSON",              (DL_FUNC) &R_readFromJSON,              6},
     {NULL, NULL, 0}
